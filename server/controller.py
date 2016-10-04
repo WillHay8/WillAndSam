bdedf9559dee36681e4b021134dbec73fd46f92f
samyoung17 <samyoung@Sams-MacBook-Pro.local>
2016-10-04 15:19:16 +0100

    Serving static content

--- server/controller.py
+++ server/controller.py
@@ -1,17 +1,17 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from random import random
 import math
 app = Flask(__name__)
 
 
-
-@app.route("/")
-def hello():
-    return "Hello World!"
-
 @app.route("/random")
-def randonNumber():
+def random_number():
 	return "Enjoy this random number on a scale from one to seventeen: " + str(int(math.ceil(random() * 17)))
 
+@app.route("/static/<path:path>")
+def serve_static_content():
+	return send_from_directory('static', path)
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run()	
